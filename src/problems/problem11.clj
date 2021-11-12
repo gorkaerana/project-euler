31538f17a29c9ac007651e3e7f42a8913039ac4f
Gorka Eraña <eranagorka@gmail.com>
2021-11-12 13:08:00 +0100

    Solved problem 11

--- src/problems/problem11.clj
+++ src/problems/problem11.clj
@@ -5,9 +5,13 @@
             [incanter.core :refer :all])
   )
 
+
 (def input-data-path "resources/problem10_grid.txt")
 
-(defn seq-to-int [seq]
+(defn seq-to-int
+  "Convert all elements of a sequence to integers. 
+  E.g. (seq-to-int (\"1\" \"2\")) --> (1 2)"
+  [seq]
   (map #(Integer. %) seq)
   )
 
@@ -22,19 +26,89 @@
     )
   )
 
-(def data-matrix
-  (->>
-   (uio/read-lines input-data-path)
-   (map #(str/split %1 #" "))
-   (map seq-to-int)
-   matrix
-   )
+(defn vertical-windows
+  ""
+  [data size]
+  (let [nrows (nrow data)
+        ncols (ncol data)
+        row-indexes (windows (range nrows) size)]
+    (for [rows row-indexes
+          col (range ncols)]
+      (sel data :rows rows :cols col)
+      )
+    )
   )
 
-;; Up-down
-(sel data-matrix :rows (range 4) :cols 0)
-;; Left-right
-(sel data-matrix :rows 0 :cols (range 4))
-;; Diagonal
-(diag (sel data-matrix :rows 0 :cols (range 4)))
+(defn horizontal-windows
+  ""
+  [data size]
+  (let [nrows (nrow data)
+        ncols (ncol data)
+        col-indexes (windows (range ncols) size)]
+    (for [cols col-indexes
+          row (range nrows)]
+      (sel data :rows row :cols cols)
+      )
+    )
+  )
 
+(defn left-to-right-diagonal-windows
+  ""
+  [data size]
+  (let [nrows (nrow data)
+        ncols (ncol data)
+        row-indexes (windows (range nrows) size)
+        col-indexes (windows (range ncols) size)]
+    (for [rows row-indexes
+          cols col-indexes]
+      (diag (sel data :rows rows :cols cols))
+      )
+    )
+  )
+
+(defn right-to-left-diagonal-windows
+  ""
+  [data size]
+  (let [nrows (nrow data)
+        ncols (ncol data)
+        row-indexes (windows (range nrows) size)
+        col-indexes (windows (range ncols) size)]
+    (for [rows row-indexes
+          cols col-indexes]
+      (diag (sel data :rows rows :cols (reverse cols)))
+      )
+    )
+  )
+
+(defn seq-prod-mapping
+  [vector]
+  {:seq (seq vector) :prod (reduce * vector)}
+  )
+
+(defn beautify-mapping
+  [mapping]
+  (str/join ""
+            ["The largest product is "
+             (format "%.0f" (:prod mapping))
+             " corresponding to adjacent numbers "
+             (vec (:seq mapping))])
+  )
+
+(def solution
+  (let [data-matrix (->>
+                     (uio/read-lines input-data-path)
+                     (map #(str/split %1 #" "))
+                     (map seq-to-int)
+                     matrix)]
+    (->>
+     (for [window-type [vertical-windows
+                        horizontal-windows
+                        left-to-right-diagonal-windows
+                        right-to-left-diagonal-windows]]
+       (apply max-key :prod (map seq-prod-mapping (window-type data-matrix 4)))
+       )
+     (apply max-key :prod)
+     beautify-mapping
+     )
+    )
+  )
