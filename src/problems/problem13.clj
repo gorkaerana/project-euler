7747c134285419455820bae53a1ea9e2f802f0ce
gorkaerana <gorka.e.r@gmail.com>
2021-12-01 11:46:28 +0100

    src/problems/problem13.clj: only use thread last macro

--- src/problems/problem13.clj
+++ src/problems/problem13.clj
@@ -4,15 +4,19 @@
   (:require [clojure.edn :as edn])
   )
 
+(defn pretty-solution [n]
+  (format
+   "The first ten digits of the sum are '%s'"
+   (subs (format "%d" n) 0 10)
+   )
+  )
+
 (def solution
-  (->
-   (->>
-    (io/read-lines "./resources/problem13_numbers.txt")
-    (map edn/read-string)
-    (reduce +)
-    biginteger
-    (format "%d")
-    )
-   (subs 0 10)
+  (->>
+   (io/read-lines "./resources/problem13_numbers.txt")
+   (map edn/read-string)
+   (reduce +)
+   biginteger
+   pretty-solution
    )
 )
